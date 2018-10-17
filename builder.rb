f1f6727ed8ff1a0f21b262793d6d9a57a1a9cb4b
Mark Roszko <mark.roszko@gmail.com>
2018-10-17 00:42:56 -0400

    Add the Jekyll header to the main adoc files. Add epub and pdf links.

--- builder.rb
+++ builder.rb
@@ -2,6 +2,7 @@
 require 'fileutils'
 require 'yaml'
 require 'asciidoctor'
+require 'tempfile'
 
 def process_compiled_docs
 	skip_folders = [
@@ -45,19 +46,39 @@ def process_compiled_docs
 			#load the asciidoc file...to get the translated title mainly
 			doc = Asciidoctor.load_file mainDocPath
 
-			guides[langEntry].push({
-									"title" => doc.doctitle, 
-									"url" => "/%s/%s.html" % [guideEntry,guideEntry],
-									"image" => "%s.png" % [guideEntry],
-									"description" => ""
-									})
 
+			image_path = File.join('img','guide-icons',guideEntry+'.png')
+
+			if !File.exist?(image_path)
+				image_path = ''
+			end
 			
 			FileUtils.mkdir_p(File.join('./', langEntry, guideEntry)) unless Dir.exist?(File.join('./', langEntry, guideEntry))
 			FileUtils.cp_r(File.join('./src', guideEntry, langEntry)+'/.', File.join('./', langEntry, guideEntry))
 
-
+			main_adoc_path = File.join('./', langEntry, guideEntry,guideEntry+'.adoc')
 			#time to mangle files!
+
+			_update_main_adoc(main_adoc_path,doc.doctitle,langEntry)
+
+			epub_path = File.join(langEntry, guideEntry,guideEntry+'.epub')
+			if !File.exist?(epub_path)
+				epub_path = ''
+			end
+
+			pdf_path = File.join(langEntry, guideEntry,guideEntry+'.pdf')
+			if !File.exist?(pdf_path)
+				pdf_path = ''
+			end
+
+			guides[langEntry].push({
+									"title" => doc.doctitle, 
+									"url" => "/%s/%s.html" % [guideEntry,guideEntry],
+									"image" => image_path,
+									"description" => "",
+									"pdf" => pdf_path,
+									"epub" => epub_path
+									})
 			
 		end
 	end
@@ -79,7 +100,6 @@ def process_compiled_docs
 		end
 	end
 
-
 	File.open("_data/generated_languages.yml", "w") { |file| file.write(outputLangHash.to_yaml) }
 
 
@@ -88,5 +108,26 @@ def process_compiled_docs
 		langIndex = indexTemplate.gsub(/%%LANG%%/, lang)
 	
 		File.open(File.join("./", lang, "index.html"), "w") {|file| file.puts langIndex }
+	end
+end
+
+def _update_main_adoc(adocPath, title, lang)
+	
+	headerTemplate = File.read("_templates/adoc_header.txt")
+	headerTemplate = headerTemplate.gsub(/%%TITLE%%/, title)
+	headerTemplate = headerTemplate.gsub(/%%LANG%%/, lang)
+	Tempfile.open File.basename(adocPath) do |tempfile|
+		# prepend data to tempfile
+		tempfile << headerTemplate
+		tempfile.write "\n"
+
+		File.open(adocPath, 'r+') do |file|
+		  # append original data to tempfile
+		  tempfile << file.read
+		  # reset file positions
+		  file.pos = tempfile.pos = 0
+		  # copy all data back to original file
+		  file << tempfile.read
+		end
 	end
 end
